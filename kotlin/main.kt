5a022e6e85c3a3e0d0d64661d73392d42142b24d
Zakirjonovi4 <smekayesh@gmail.com>
2022-08-30 17:30:18 +0300

    How to fix partials?

--- kotlin/main.kt
+++ kotlin/main.kt
@@ -20,23 +20,36 @@ fun main(){
         println("Введите предполагаемое число: ")
         numberUser = scan.nextLine().trim().toInt() // Считали первое число
         numberUserStr = numberUser.toString()
+        if (numberUser <= 999) return
 
-        if (numberUser in 0..999) return
+       /* Проверяем полные совпадения, с неполными еще есть проблемы*/
 
         if (numberUserStr[0] == numberString[0]) complete++
+            else if((numberUserStr[0] == numberString[1]) || (numberUserStr[0] == numberString[2]) || (numberUserStr[0] == numberString[3])){
+                partial++
+            }
         if (numberUserStr[1] == numberString[1]) complete++
+           else if((numberUserStr[1] == numberString[0]) || (numberUserStr[1] == numberString[2]) || (numberUserStr[1] == numberString[3])){
+                partial++
+           }
         if (numberUserStr[2] == numberString[2]) complete++
+            else if((numberUserStr[2] == numberString[0]) || (numberUserStr[2] == numberString[1]) || (numberUserStr[2] == numberString[3])){
+                partial++
+            }
         if (numberUserStr[3] == numberString[3]) complete++
+            else if((numberUserStr[3] == numberString[0]) || (numberUserStr[3] == numberString[1]) || (numberUserStr[3] == numberString[2])){
+                partial++
+            }
 
 
-        if ((numberUserStr[0] == numberString[1]) || (numberUserStr[0] == numberString[2]) || (numberUserStr[0] == numberString[3])) partial++
-        if ((numberUserStr[1] == numberString[0]) || (numberUserStr[1] == numberString[2]) || (numberUserStr[1] == numberString[3])) partial++
-        if ((numberUserStr[2] == numberString[0]) || (numberUserStr[2] == numberString[1]) || (numberUserStr[2] == numberString[3])) partial++
-        if ((numberUserStr[3] == numberString[0]) || (numberUserStr[3] == numberString[1]) || (numberUserStr[3] == numberString[2])) partial++
-        
+
+/*        if (((numberUserStr[0] == numberString[1]) || (numberUserStr[0] == numberString[2]) || (numberUserStr[0] == numberString[3]))) partial++
+            else if (((numberUserStr[1] == numberString[0]) || (numberUserStr[1] == numberString[2]) || (numberUserStr[1] == numberString[3]))) partial++
+                else if (((numberUserStr[2] == numberString[0]) || (numberUserStr[2] == numberString[1]) || (numberUserStr[2] == numberString[3]))) partial++
+                    else if (((numberUserStr[3] == numberString[0]) || (numberUserStr[3] == numberString[1]) || (numberUserStr[3] == numberString[2]))) partial++*/
+        if ((partial-complete) >= 0 ) partial -= complete
+
         println("Колличество полных совпадений: $complete, частичных совпадений: $partial")
-
-
     }
 
     println("Вы выйграли! Правильно число $randomNum")
